fefce145792f6347d4b19b96bc50a3c6386cf96f
LearningTask <learningtask2021.1@gmail.com>
2022-03-02 18:03:02 +0530

    added lambda rest end points for getAllUSers and getAllTweets

--- src/main/java/com/tweetApp/TweetApplication.java
+++ src/main/java/com/tweetApp/TweetApplication.java
@@ -1,16 +1,42 @@
 package com.tweetApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.tweetApp.model.Tweets;
+import com.tweetApp.model.Users;
+import com.tweetApp.repository.UsersRepository;
+import com.tweetApp.services.TweetsService;
+
+import java.util.*;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class TweetApplication {
+	
+	@Autowired
+	public UsersRepository repo;
+	@Autowired 
+	public TweetsService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TweetApplication.class, args);
+	}
+	
+	@Bean
+	public Supplier<List<Users>> getAllUsers(){
+		System.out.println("Inside getAllUsers lambda function");
+		return ()-> repo.findAll();
+	}
+	
+	@Bean
+	public Supplier<List<Tweets>> getAllTweets(){
+		System.out.println("Inside getting all tweets lambda function");
+		return ()->service.retrunAllTweets();
 	}
 
 }
---
+++ src/main/java/com/tweetApp/lamda/lambdaRequestHandler.java
@@ -0,0 +1,7 @@
+package com.tweetApp.lamda;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+@SuppressWarnings("deprecation")
+public class lambdaRequestHandler extends SpringBootRequestHandler<String,Object>{
+
+}
