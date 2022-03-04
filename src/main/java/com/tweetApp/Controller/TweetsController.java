0f0ede10f2a5af39310b09ea51595f7447eee315
LearningTask <learningtask2021.1@gmail.com>
2022-03-04 17:02:44 +0530

    allowed cross origin from all the domains

--- src/main/java/com/tweetApp/Controller/TweetsController.java
+++ src/main/java/com/tweetApp/Controller/TweetsController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1.0/tweets")
 public class TweetsController
